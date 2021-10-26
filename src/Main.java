52a348d869790cce533c0e4367d9d80a73751d6b
Trent Bultsma <trentbultsma@gmail.com>
2021-10-25 23:59:04 -0700

    sanitize user inputs

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main {
@@ -24,36 +25,96 @@ public class Main {
                     \tQuit the program (Q)
                     """);
 
-            command = console.next().toUpperCase();
+            command = console.nextLine().toUpperCase();
 
             switch (command) {
                 case "I":
 
-                    System.out.print("How many rows? ");
-                    rows = console.nextInt();
-                    System.out.print("How many columns? ");
-                    cols = console.nextInt();
+                    // get size of the matrix (rows, columns)
+                    // while loops are to ensure a validly sized matrix
+
+                    while (true) {
+                        System.out.print("How many rows? ");
+                        String input = console.nextLine();
+
+                        try {
+                            rows = Integer.parseInt(input);
+                            if (rows > 0) {
+                                break;
+                            } else {
+                                System.out.println("Invalid input, please chose a number larger than 1");
+                            }
+                        } catch (NumberFormatException e) { // if the user doesn't input an int
+                            System.out.println("Invalid input, please input an integer");
+                        }
+                    }
+                    while (true) {
+                        System.out.print("How many columns? ");
+                        String input = console.nextLine();
+
+                        try {
+                            cols = Integer.parseInt(input);
+                            if (cols > 0) {
+                                break;
+                            } else {
+                                System.out.println("Invalid input, please chose a number larger than 1");
+                            }
+                        } catch (NumberFormatException e) { // if the user doesn't input an int
+                            System.out.println("Invalid input, please input an integer");
+                        }
+                    }
 
                     m = new Matrix(rows, cols);
 
                     String line;
                     Scanner lineScanner;
-                    console.nextLine(); // clear empty line from scanner
 
+                    // get each row inputted
                     System.out.println("Input each value in a row by typing each number separated by spaces:");
 
                     for (int i = 0; i < rows; i++) {
 
-                        System.out.print("Row " + (i + 1) + ": ");
-                        line = console.nextLine();
-                        lineScanner = new Scanner(line);
+                        // while loop to make sure each row is inputted correctly, or ask again until one is
+                        // correctly inputted
+                        while (true) {
+                            System.out.print("Row " + (i + 1) + ": ");
+                            line = console.nextLine();
+                            lineScanner = new Scanner(line);
+                            String token;
+                            double value;
+                            boolean inputError = false;
 
-                        for (int j = 0; j < cols; j++) {
+                            for (int j = 0; j < cols; j++) {
 
-                            double value = lineScanner.nextDouble();
+                                try { // sends an error message when there weren't enough entries inputted
+                                    token = lineScanner.next();
+                                } catch (NoSuchElementException e) {
+                                    System.out.println("Invalid input, make sure to input the same amount of entries" +
+                                            " as there are columns in the matrix (" + cols + ")");
+                                    inputError = true;
+                                    break;
+                                }
+                                try {
+                                    value = Double.parseDouble(token);
+                                } catch (NumberFormatException e) {
+                                    System.out.println("Invalid input, please only include numbers");
+                                    inputError = true;
+                                    break;
+                                }
 
-                            m.set(i, j, value);
+                                m.set(i, j, value);
 
+                            }
+                            if (!inputError) {
+                                if (!lineScanner.hasNext()) {
+                                    break;
+                                } else {
+                                    // sends an error message when too many entries were inputted
+                                    // but only if the user didn't provide an invalid input
+                                    System.out.println("Invalid input, make sure to input the same amount of entries" +
+                                            " as there are columns in the matrix (" + cols + ")");
+                                }
+                            }
                         }
 
                         lineScanner.close();
